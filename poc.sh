bcb25091a8840d3c24252b12491c156f26a04021
BedtimeXXX <165554897+BedtimeXXX@users.noreply.github.com>
2024-12-01 11:18:57 +0000

    Update poc.sh

--- poc.sh
+++ poc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Define repository paths
-HULK_REPO="git@github.com:safebuffer/hulk.git"
-pullme_REPO="git@github.com:safebuffer/submod.git"
+HULK_REPO="https://github.com/BedtimeXXX/HTB/hulk.git"
+pullme_REPO="https://github.com/BedtimeXXX/HTB/submod.git"
 
 # Final Exploit Repo
-SMASH_REPO="git@github.com:safebuffer/smash.git"
+SMASH_REPO="https://github.com/BedtimeXXX/HTB/smash.git"
 
 # Function to clone and set up the hook repository
 setup_HULK_REPO() {
